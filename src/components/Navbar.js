db1622ce068e14074a7dfea3608463a60d56db3c
NunezAndy <andy_nunez91@yahoo.com>
2024-01-10 08:06:13 -0600

    recent changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+       <div className="toggleButton">
+        <button></button>
+       </div>
+       <div className="links">
+         <Link to="/"> Home </Link>
+         <Link to="/projects"> Projects </Link>
+         <Link to="/skills"> Skills </Link>
+         <Link to="/contact"> Contact </Link>
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
