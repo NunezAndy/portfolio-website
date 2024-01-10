28005488aa508d256bce409e1fd2da071562f45d
NunezAndy <andy_nunez91@yahoo.com>
2024-01-10 10:16:29 -0600

    skills changes

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import "../styles/Skills.css";
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div className="skills">
+      <h1> Skills </h1>
+      <ol className="list">
+        <li className="item">
+          <h2> Front-End </h2>
+          <span> JavaScript, React.JS, NPM, Three.JS, HTML5, CSS3 Material UI, Bootstrap </span>
+        </li>
+        <li className="item">
+          <h2> Back-End </h2>
+          <span> Node.JS, Express.JS, Python, Flask, PostgreSQL, MongoDB, MySQL </span>
+        </li>
+        <li className="item">
+          <h2> Tools </h2>
+          <span> GitHub, Git, VS Code, Eclipse, Insomnia, Postico </span>
+        </li>
+      </ol>
+    </div>
   )
 }
 
