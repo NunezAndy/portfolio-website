95954eb1cfdaeadf99acc386cf044b7c1d82a245
NunezAndy <andy_nunez91@yahoo.com>
2024-01-10 09:38:56 -0600

    contact changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import "../styles/Footer.css";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="footer">
+      <div className="socialMedia">
+        <a
+          href="https://www.linkedin.com/in/andy-nunez-webdev/"
+          target="_blank"
+          rel=""
+          className="socialMedia"
+        >
+          <LinkedInIcon />
+        </a>
+        <a
+          href="https://github.com/NunezAndy"
+          target="_blank"
+          rel=""
+          className="socialMedia"
+        >
+          <GitHubIcon />
+        </a>
+      </div>
+      <div className="socialMedia">
+        <EmailIcon />
+      </div>
+      <p> <b>made in Chicago!</b> </p>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
