ce23ac0907d96be119f9cb70c13f80b6ebc4375a
NunezAndy <andy_nunez91@yahoo.com>
2024-01-10 09:54:27 -0600

    home changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+        <div className="about">
+            <h2> Hi, My Name is Andy Nunez</h2>
+            <div className="prompt">
+                <p>As a professional web developer I'm responsible for designing, creating, and maintaining websites and web applications. My primary focus has been, and always will be on the technical aspects of a website's functionality, user experience, and overall performance.</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
